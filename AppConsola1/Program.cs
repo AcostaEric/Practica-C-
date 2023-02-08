728b54f468fe344d13d6f086f2d76a6d48cf08e3
Acosta Eric <acostaeric1508@gmail.com>
2023-02-08 20:05:41 -0300

    agrego consola 1

--- AppConsola1/Program.cs
+++ AppConsola1/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-d awkdop akwdpo kadpoa eawpeo awoke opawek
+﻿//escribir un programa que haga lo siguiente:
+
+//1) borrar la pantalla.
+//2) pedir el nombre de una persona.
+//3) saludarlo con un texto que diga "¡hola [nombreingresado]!"
+//4) preguntar si se quiere continuar.
+//5) si la respuesta es "s" repetir desde el punto 1.
+//6) si la respuesta es "n" finalizar el programa mostrando un mensaje que diga "programa finalizado correctamente".
+//7) si la respuesta no es ni "s" ni "n" que tambien finalice el programa, pero mostrando un mensaje que diga "opcion no valida".
+
+string respuesta = "S";
+
+while (respuesta.ToUpper() == "S") {
+    Console.Clear();
+
+    Console.WriteLine("Buenas! Ingrese su nombre");
+    var nombreIngresado = Console.ReadLine();
+
+    Console.WriteLine($"Hola! {nombreIngresado}!");
+
+    Console.WriteLine("Desea continuar? S para si, N para no.");
+    respuesta = Console.ReadLine();
+         };
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente.");
+
+}
+else { Console.WriteLine("Opcion no valida"); 
+}
